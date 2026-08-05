7d9a6e1b9c693c3caacac01487acca098aa41a8e
KAR Developer <dev@hellokar.com>
2026-08-05 00:42:10 -0400

    feat: add Metricool site tracking

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
+const metricoolScript = document.createElement('script')
+metricoolScript.type = 'text/javascript'
+metricoolScript.src = 'https://tracker.metricool.com/resources/be.js'
+metricoolScript.onload = () => {
+  const tracker = (window as Window & { beTracker?: { t: (options: { hash: string }) => void } }).beTracker
+  tracker?.t({ hash: '8d9f50f8b2d9334325867165e592dc11' })
+}
+document.head.appendChild(metricoolScript)
+
 createRoot(document.getElementById('root')!).render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>)
